eed51b04c25b3eb6fdb0570d5aba27ecb2acc297
Apitran <59908627+Apitran@users.noreply.github.com>
2020-03-24 09:52:37 +0700

    3rd

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <BODY BGCOLOR=#FFFFFF>
 <H1>
 <?php echo "รหัสนิสิต-62310136"; ?><br>
-<?php echo "ชื่อ-สกุล นาย........."; ?><br>
+<?php echo "ชื่อ-สกุล นาย อภิธาร บู่สามสาย"; ?><br>
 <iframe width="500" height="500"
 src="https://www.youtube.com/embed/RgKAFK5djSk?autoplay=1">
 </iframe>
