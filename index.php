9ab1073b590310bc008091f48ce768e5d70343f2
Apitran <59908627+Apitran@users.noreply.github.com>
2020-03-24 10:32:38 +0700

    6th

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <H1>
 <?php echo "รหัสนิสิต-62310136"; ?><br>
 <?php echo "ชื่อ-สกุล นาย อภิธาร บู่สามสาย"; ?><br>
-<iframe width="500" height="800"
-src="https://youtu.be/erjDimJe9UA">
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/erjDimJe9UA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </BODY>
 </HTML>
